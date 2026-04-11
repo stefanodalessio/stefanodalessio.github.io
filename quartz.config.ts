ec0234a829f7d70b6ffc19c524c796cfab52e6d0
stefanodalessio <8409032+stefanodalessio@users.noreply.github.com>
2026-04-11 11:15:34 +0200

    Quartz sync: Apr 11, 2026, 11:15 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "STEFANO     D'ALESSIO",
+    pageTitle: "STEFANO        D'ALESSIO",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
